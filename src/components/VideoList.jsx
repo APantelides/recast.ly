eb279649af50f6e83b7f8e514eb6154f63ca8f3a
apantelides <alex.pantelides@gmail.com>
2016-08-12 16:23:59 -0700

    Pass VideoList and VideoListEntry specs

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,16 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+  {  
+    props.videos.map(video => {
+      console.log(video.snippet.thumbnails.default.url);
+      return (
+      <VideoListEntry video={video}/>
+      );
+    })
+   
+      
+  }
+   
   </div>
 );
 
@@ -21,3 +23,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+//  {props.videos.map(video => {  })}
