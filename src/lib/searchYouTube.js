feab2cc1054895259027238dc680caf04a0df963
apantelides <alex.pantelides@gmail.com>
2016-08-13 11:14:42 -0700

    Implement searchyoutube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      type: 'video'
+    },
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(error) {
+      console.log('error occurred on get', error);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
